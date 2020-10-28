c0b2b5ffd39026965338c2f4907d1805be15d37d
Sean Choi <seanchoi@seans-mac-mini.myfiosgateway.com>
2020-10-28 18:56:22 -0400

    reponsive update

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -25,12 +25,13 @@ with open(BASE_DIR / 'secret.json') as f:
 # SECURITY WARNING: keep the secret key used in production secret!
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+# DEBUG = False
+DEBUG = True
 
 # SECRET_KEY = 'c4--qvpd2npg16fprmf3fd)z_(epx6hol5@kwc0+ka9dmi)=y)'
 
-ALLOWED_HOSTS = ['3.132.252.161', 'www.jongwoochoi.com']
-
+# ALLOWED_HOSTS = ['3.132.252.161', 'www.jongwoochoi.com']
+ALLOWED_HOSTS = []
 
 # Application definition
 
@@ -127,10 +128,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'static'
-# STATICFILES_DIRS = [
-#     BASE_DIR / 'static',
-# ]
+# STATIC_ROOT = BASE_DIR / 'static'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
 
 MEDIA_ROOT = BASE_DIR / 'media'
 MEDIA_URL = '/media/'
